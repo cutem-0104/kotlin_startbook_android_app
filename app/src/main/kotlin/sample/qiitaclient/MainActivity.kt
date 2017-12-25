546f251b350bfa8dbe27e26be9b146d490eb9998
shuhei.sugaya <shuhei.sugaya>
2017-12-26 01:27:56 +0900

    RxLifeCycle,ProgressBer導入 Kotlinスタートブックp280まで

--- app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
@@ -1,12 +1,15 @@
 package sample.qiitaclient
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.ProgressBar
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.GsonBuilder
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity
+import com.trello.rxlifecycle.kotlin.bindToLifecycle
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
@@ -16,18 +19,19 @@ import sample.qiitaclient.client.ArticleClient
 import sample.qiitaclient.model.Article
 import sample.qiitaclient.model.User
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : RxAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val listAdapter = ArticleListAdapter(applicationContext)
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
 
         listAdapter.articles = listOf(dummyArticle("Kotlin入門","たろう"),
                 dummyArticle("Java入門", "じろう"))
 
-        val listView: ListView = findViewById<ListView>(R.id.list_view)
+        val listView = findViewById<ListView>(R.id.list_view)
         listView.adapter = listAdapter
 
         listView.setOnItemClickListener { articleView, view, position, id ->
@@ -52,9 +56,15 @@ class MainActivity : AppCompatActivity() {
         val searchButton = findViewById<Button>(R.id.search_button)
 
         searchButton.setOnClickListener {
+            progressBar.visibility = View.VISIBLE
+
             articleClient.search(queryEditText.text.toString())
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
+                    .doAfterTerminate {
+                        progressBar.visibility = View.GONE
+                    }
+                    .bindToLifecycle(this)
                     .subscribe({
                         queryEditText.text.clear()
                         listAdapter.articles = it
